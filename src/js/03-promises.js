9c4e4bd12ef3059137fde3723b07128eae820c42
ciehgiordani <acieh_giordani@yahoo.com>
2024-02-10 11:28:07 +0800

    2nd commit
    promises modif

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,3 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -19,8 +11,10 @@ function createPromise(position, delay) {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
+         // Fulfill
         resolve({ position, delay });
       } else {
+        //Reject
         reject({ position, delay });
       }
     }, delay);
